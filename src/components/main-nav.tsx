46147a59dc060f18e526a149a1a0addf0936331d
Ameer Hamza <ah8831912@gmail.com>
2025-10-19 15:28:47 +0000

    Try fixing this error: `Console Error: In HTML, <a> cannot be a descenda

--- src/components/main-nav.tsx
+++ src/components/main-nav.tsx
@@ -35,22 +35,20 @@ export function MainNav() {
       <SidebarMenu>
         {links.map((link) => (
           <SidebarMenuItem key={link.href}>
-            <Link href={link.href} passHref>
-              <SidebarMenuButton
-                asChild
-                isActive={
-                  link.href === '/'
-                    ? pathname === link.href
-                    : pathname.startsWith(link.href)
-                }
-                tooltip={link.label}
-              >
-                <a>
-                  <link.icon />
-                  <span>{link.label}</span>
-                </a>
-              </SidebarMenuButton>
-            </Link>
+            <SidebarMenuButton
+              asChild
+              isActive={
+                link.href === '/'
+                  ? pathname === link.href
+                  : pathname.startsWith(link.href)
+              }
+              tooltip={link.label}
+            >
+              <Link href={link.href}>
+                <link.icon />
+                <span>{link.label}</span>
+              </Link>
+            </SidebarMenuButton>
           </SidebarMenuItem>
         ))}
       </SidebarMenu>

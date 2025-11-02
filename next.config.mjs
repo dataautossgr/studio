30f84ae0fc7adb70bdf71eeb4dbec647a9ae2c9e
Ameer Hamza <ah8831912@gmail.com>
2025-11-02 16:13:34 +0000

    deployment me error arha hi Error: Command "npm run build" exited with 1

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
       }
     ],
   },

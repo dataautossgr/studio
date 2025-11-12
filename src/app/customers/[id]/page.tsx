ce1531f0bcd0d0250d026acd061869e655e5697e
Ameer Hamza <ah8831912@gmail.com>
2025-11-12 15:59:08 +0000

    Try fixing this error: `Runtime Error: ReferenceError: CustomerLedgerDet

--- src/app/customers/[id]/page.tsx
+++ src/app/customers/[id]/page.tsx
@@ -1,4 +1,5 @@
 
+import CustomerLedgerDetail from './customer-detail';
 
 // This is now a Server Component. It can pass params to a Client Component.
 export default function CustomerLedgerPage({ params }: { params: { id: string } }) {
